99bceafb60ba8910ded2bc64ddec0d019684c183
wael senani <111441399+wael-boss@users.noreply.github.com>
2023-01-26 22:25:28 +0100

    done some styling on the exercises containers

---
+++ src/components/ExeciseFocus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExeciseFocus = () => {
+  return (
+    <div>ExeciseFocus</div>
+  )
+}
+
+export default ExeciseFocus
--- src/components/ExercisesLoop.js
+++ src/components/ExercisesLoop.js
@@ -1,11 +1,14 @@
-
+import {BsArrowRight} from 'react-icons/bs' 
 const ExercisesLoop = ({exercises}) => {
   return (
     <div className="exerciseContainer">
     {exercises.map(exercise=>{
+      console.log(exercise)
         return (
             <div className="exercise" key={exercise.Name}>
+                <img src={`/images/${exercise.Force}.png`}/>
                 <p>{exercise.Name}</p>
+                <button><BsArrowRight/></button>
             </div>
         )
     })}
